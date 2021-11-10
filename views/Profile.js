4ca2a828d5b1e1e37eeaeb2b707e775bcb8980b2
Ricardo <r_luna99@hotmail.com>
2021-11-09 17:34:56 -0700

    Visual componentes from the profile screen were modified

--- views/Profile.js
+++ views/Profile.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, View, Text, Button } from 'react-native';
+import { StyleSheet, View, Text, Pressable } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { AuthContext } from '../context'
 
@@ -24,47 +24,49 @@ export default function Profile({ navigation }) {
         <View style={styles.container}>
 
             {/* <Text>{token}</Text> */}
-            <Button
+            <Pressable
                 onPress={() => {navigation.navigate('Info') }}
-                title="Info restaurante"
-                color="#333"
-                accessibilityLabel="Información del restaurante"
-                style={styles.wideButton}
-            />
-            <View
-                style={{
-                    marginBottom: 10
-                }}
-            />
-            <Button
-                onPress={() => { navigation.navigate('Menu')}}
-                title="Menu"
-                color="#000"
-                accessibilityLabel="Menú del restaurante"
-                style={styles.wideButton}
-            />
-            <View
-                style={{
-                    marginBottom: 20,
-                }}
-            />
-            <Button
-                onPress={() => { navigation.navigate('Bank')}}
-                title="Registrarse para cobro digital"
-                color="#333"
-                style={styles.wideButton}
-            />
-            <View
-                style={{
-                    marginBottom: 70,
-                }}
-            />
-            <Button
-                onPress={() => { logOut() }}
-                title="Cerrar sesión"
-                color="red"
-                accessibilityLabel="Cerrar sesión"
-            />
+                style={[styles.pressableArea, styles.firstButton]}
+            >
+                <Text
+                    style={[styles.text, styles.firstText]}
+                >
+                    Información del restaurante
+                </Text>
+            </Pressable>
+
+            <Pressable
+                onPress={ () => { navigation.navigate('Menu') }}
+                style={[styles.pressableArea, styles.secondButton]}
+            >
+                <Text
+                    style={[styles.text, styles. secondText]}
+                >
+                    Menú del restaurante
+                </Text>
+            </Pressable>
+
+            <Pressable
+                onPress={ () => { navigation.navigate('Bank') }}
+                style={[styles.pressableArea, styles.thirdButton]}
+            >
+                <Text
+                    style={[styles.text, styles. thirdText]}
+                >
+                    Cobro digital
+                </Text>
+            </Pressable>
+
+            <Pressable
+                onPress={ () => { logOut() }}
+                style={[styles.pressableArea, styles.fourthButton]}
+            >
+                <Text
+                    style={[styles.text, styles. fourthText]}
+                >
+                    Cerrar sesión
+                </Text>
+            </Pressable>
         </View>
 
     );
@@ -72,9 +74,62 @@ export default function Profile({ navigation }) {
 
 const styles = StyleSheet.create({
     container: {
-
         marginTop: 20
     },
 
+    pressableArea: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginLeft: 10,
+        marginRight: 10,
+        marginBottom: 10,
+        paddingLeft: 10,
+        paddingTop: 15,
+        paddingBottom: 15,
+        borderRadius: 10,
+        backgroundColor: "#fff",
+        borderBottomWidth: 1,
+        borderRightWidth: 1,
+    },
 
+    firstButton: {
+        borderBottomColor: "#00A5E3",
+        borderRightColor: "#00A5E3",
+    },
+    
+    secondButton: {
+        borderBottomColor: "#C05780",
+        borderRightColor: "#C05780",
+    },
+    
+    thirdButton: {
+        borderBottomColor: "#4DD091",
+        borderRightColor: "#4DD091",
+    },
+
+    fourthButton: {
+        borderBottomColor: "#FC6238",
+        borderRightColor: "#FC6238",
+    },
+
+    text: {
+        fontSize: 16,
+        fontWeight: 'bold'
+    },
+
+    firstText: {
+        color: "#00A5E3"
+    },
+
+    secondText: {
+        color: "#C05780"
+    },
+
+    thirdText: {
+        color: "#4DD091"
+    },
+
+    fourthText: {
+        color: "#FC6238"
+    }
 });
